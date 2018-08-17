2f166a7bce9ae3c7028dce7b5ee02fa15d887e78
Jhoni Costa <jhoni_costa@outlook.com>
2018-08-17 15:06:23 -0300

    Crindo Pagina de detalhes do produto a

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
-import { ProdutoService } from '../../services/produto.service';
 import { API_CONFIG } from '../../config/api.config';
+import { ProdutoService } from '../../services/produto.service';
 
 @IonicPage()
 @Component({
@@ -20,28 +20,27 @@ export class ProdutosPage {
   }
 
   ionViewDidLoad() {
-
     let categoria_id = this.navParams.get('categoria_id');
     this.produtoService.findByCategoria(categoria_id)
-    .subscribe(response =>{
+      .subscribe(response => {
         this.items = response['content'];
         this.loadImageUrls();
-    },
-    error => {}); 
+      },
+      error => {});
   }
 
-  loadImageUrls(){
-    for(var i=0; i<this.items.length; i++){
+  loadImageUrls() {
+    for (var i=0; i<this.items.length; i++) {
       let item = this.items[i];
       this.produtoService.getSmallImageFromBucket(item.id)
-      .subscribe(response => {
-        item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
-      },
-    error => {});
+        .subscribe(response => {
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+        },
+        error => {});
     }
-  }
+  }  
 
-  showDetail(){
+  showDetail() {
     this.navCtrl.push('ProdutoDetailPage');
   }
 }
